e1e3a3391275d7176346d8dd4a0d06d79e79154c
Jorik125 <103557538+Godstex@users.noreply.github.com>
2023-10-25 00:29:35 +0300

    Сделал наследуемую шапку и подвал

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -10,6 +10,8 @@ use yii\bootstrap5\Html;
 use yii\bootstrap5\Nav;
 use yii\bootstrap5\NavBar;
 
+const URL = 'http://localhost:8081/site/';
+
 AppAsset::register($this);
 
 $this->registerCsrfMetaTags();
@@ -25,57 +27,71 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
 <head>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 </head>
-<body class="d-flex flex-column h-100">
+<body>
 <?php $this->beginBody() ?>
 
-<header id="header">
-    <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => ['class' => 'navbar-expand-md navbar-dark bg-dark fixed-top']
-    ]);
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav'],
-        'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
-            Yii::$app->user->isGuest
-                ? ['label' => 'Login', 'url' => ['/site/login']]
-                : '<li class="nav-item">'
-                    . Html::beginForm(['/site/logout'])
-                    . Html::submitButton(
-                        'Logout (' . Yii::$app->user->identity->username . ')',
-                        ['class' => 'nav-link btn btn-link logout']
-                    )
-                    . Html::endForm()
-                    . '</li>'
-        ]
-    ]);
-    NavBar::end();
-    ?>
-</header>
+<div class="container">
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+      <div class="col-md-3 mb-2 mb-md-0">
+        <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+            <img width="110" height="100" src="<?= \yii\helpers\Url::to('img/logo.png')?>" alt="Logo">
+        </a>
+      </div>
 
-<main id="main" class="flex-shrink-0" role="main">
+        <div>
+            <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+                <?php if ((yii\helpers\Url::current([], true) == URL.'index')): ?>
+                    <li><a href="#" style="font-size: 22px; font-family: Inter; font-weight: 400; text-decoration: underline; word-wrap: break-word; color: black" class="px-2 nav-link">Главная</a></li>
+                    <li><a href="#" style="font-size: 22px; font-family: Inter; font-weight: 400; word-wrap: break-word; color: black" class="nav-link px-2">Забронировать</a></li>
+                    <li><a href="#" style="font-size: 22px; font-family: Inter; font-weight: 400; word-wrap: break-word; color: black" class="nav-link px-2">Контакты</a></li>
+                <?php endif;?>
+            </ul>
+        </div>
+
+      <div class="col-md-3 text-end">
+        <button type="button" style="width: 100px" class="btn btn btn-dark">Войти</button>
+      </div>
+    </header>
+  </div>
+
     <div class="container">
-        <?php if (!empty($this->params['breadcrumbs'])): ?>
-            <?= Breadcrumbs::widget(['links' => $this->params['breadcrumbs']]) ?>
-        <?php endif ?>
-        <?= Alert::widget() ?>
         <?= $content ?>
     </div>
-</main>
 
-<footer id="footer" class="mt-auto py-3 bg-light">
-    <div class="container">
-        <div class="row text-muted">
-            <div class="col-md-6 text-center text-md-start">&copy; My Company <?= date('Y') ?></div>
-            <div class="col-md-6 text-center text-md-end"><?= Yii::powered() ?></div>
-        </div>
+<div class="container">
+  <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
+    <div class="col mb-3">
+      <a href="/" class="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none">
+        FOTOCHKA STUDIO
+      </a>
+      <p class="text-body-secondary">Воплощай мечты в фотографии!</p>
     </div>
-</footer>
+
+
+    <div class="col mb-3">
+
+    </div>
+
+    <div class="col mb-3">
+
+    </div>
+
+      <div class="col mb-3">
+
+    </div>
+
+      <div class="col mb-3">
+      <h5>Разделы</h5>
+      <ul class="nav flex-column">
+        <li class="nav-item mb-2"><a href="#" style="font-size: 22px; font-family: Inter; font-weight: 400; word-wrap: break-word; color: gray"  class="nav-link p-0 text-body-secondary">Главная</a></li>
+        <li class="nav-item mb-2"><a href="#" style="font-size: 22px; font-family: Inter; font-weight: 400; word-wrap: break-word; color: gray"  class="nav-link p-0 text-body-secondary">Бронирование</a></li>
+        <li class="nav-item mb-2"><a href="#" style="font-size: 22px; font-family: Inter; font-weight: 400; word-wrap: break-word; color: gray"  class="nav-link p-0 text-body-secondary">Контакты</a></li>
+      </ul>
+    </div>
+  </footer>
+</div>
 
 <?php $this->endBody() ?>
 </body>
